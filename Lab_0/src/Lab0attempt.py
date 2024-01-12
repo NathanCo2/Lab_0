bfc17f7bb6a3646c0db56093b589e43227cb4115
jpere180 <156122419+jpere180@users.noreply.github.com>
2024-01-11 22:44:23 -0800

    Lab0attempt

--- Lab_0/src/Lab0attempt.py
+++ Lab_0/src/Lab0attempt.py
@@ -4,19 +4,22 @@
   """
 import pyb
 import utime
+from cqueue import IntQueue as Queue
 import micropython
 micropython.alloc_emergency_exception_buf(100)
 
 # Make pins/ADC Objects (aka initialize pins)
 pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)# (Pin.OUT_PP) configures the pin for output, with push-oull control
-adc0 = pyb.ADC(pyb.Pin.board.PC0) #To read the ADC 
+pinB0 = pyb.Pin(pyb.Pin.board.PB0, pyb.Pin.OUT_PP)
+
+adc0 = pyb.ADC(pinB0) #To read the ADC 
 
   def timer_int(tim_num):
       """!
       Doxygen style docstring for interrupt callback function
       """
-      adcpin = pyb.ADC (pyb.Pin.board.PC0)# Reads ADC
-      def cqueue.IntQueue.put(int data)# Stores data in queue 
+      adcpin = pyb.ADC (pinB0)# Reads ADC
+      Queue.put(int data)# Stores data in queue 
 
   def step_response (...parameters...):
       """!
@@ -24,10 +27,13 @@ adc0 = pyb.ADC(pyb.Pin.board.PC0) #To read the ADC
       """
       adc = pyb.ADC(pyb.Pin.board.PC0)#creates an ADC on pin PB0
       tim = pyb.Timer(1, freq=100)#creates a timer running (turns on callback?)
+      tim.callback(timer_int)#turns on callback
       pinC0.high()#sets pin high to trigger step
-    
-      val = adc.read_vref()#reads MCU supply voltage
-      print ("End")
+      if Queue.full()#checks if the queue is full
+      tim.callback(NONE)#turns off the callback
+      for data in Queue:
+          print(tim, data)#prints data line by line
+      print ("End")#print End
       pinC0.low()# Resets the Pin to run again 
 
 
